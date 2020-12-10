ffe866624cae955b0f8a33e5742a454b90516315
akine <weezer.and.you@gmail.com>
2020-12-10 23:28:01 +0900

    シェルプログラミング

---
+++ my-first.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ls
+# date
+# echo メッセージを入力してください。
+# read message
+# echo 入力されたメッセージ： $message
+read -p "日本で二番目に高い山は槍ヶ岳でしょうか？ [y/n]" yn
+if [ $yn = "n" ]; then
+  echo 正解です。日本で二番目に高い山は北岳です。
+else
+  echo 不正解です。日本で二番目に高い山は北岳です。
+fi
